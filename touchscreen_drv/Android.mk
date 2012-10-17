415ee866309c5774cf317ef2f053e2a3a35a145b
Dees_Troy <dees_troy@teamw.in>
2012-10-17 11:12:18 -0400

    Update config

--- touchscreen_drv/Android.mk
+++ touchscreen_drv/Android.mk
@@ -7,15 +7,15 @@ include $(CLEAR_VARS)
 #
 #LOCAL_C_INCLUDES:= uim.h
 
-LOCAL_MODULE:=ts_srv
-LOCAL_MODULE_TAGS := eng
-LOCAL_MODULE_CLASS := RECOVERY_EXECUTABLES
-LOCAL_MODULE_PATH := $(TARGET_RECOVERY_ROOT_OUT)/sbin
+#LOCAL_MODULE:=ts_srv
+#LOCAL_MODULE_TAGS := eng
+#LOCAL_MODULE_CLASS := RECOVERY_EXECUTABLES
+#LOCAL_MODULE_PATH := $(TARGET_ROOT_OUT)/sbin
 
-LOCAL_SRC_FILES:= \
-	ts_srv.c \
-	digitizer.c
-LOCAL_CFLAGS:= -g -c -W -Wall -O2 -mtune=cortex-a9 -mfpu=neon -mfloat-abi=softfp -funsafe-math-optimizations -D_POSIX_SOURCE -I/home/green/touchpad/hp_tenderloin_kernel/include
+#LOCAL_SRC_FILES:= \
+#	ts_srv.c \
+#	digitizer.c
+#LOCAL_CFLAGS:= -g -c -W -Wall -O2 -mtune=cortex-a9 -mfpu=neon -mfloat-abi=softfp -funsafe-math-optimizations -D_POSIX_SOURCE -I/home/green/touchpad/hp_tenderloin_kernel/include
 
-include $(BUILD_EXECUTABLE)
+#include $(BUILD_EXECUTABLE)
 
